4fbbb7d7748f8217fed914c976c8e73c6150cc2b
brevw <tlili.ahmed.mo@gmail.com>
2025-04-26 23:35:17 -0400

    zsh + iterm

--- install.sh
+++ install.sh
@@ -2,15 +2,53 @@
 set -e
 
 DOTFILES_DIR="$HOME/dotfiles"
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
 
 echo "Installing config files and dependencies"
 
-# install dependencies and link the config files
+## install global dependencies
 
+# Check if Homebrew is installed
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew is not installed. Installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed."
+fi
+
+if ! command_exists git; then
+    echo "Installing Git..."
+    brew install git
+else
+    echo "Git is already installed."
+fi
+
+## install local dependencies and link the config files
+mkdir -p "$HOME/.config"
+
+#-->>>>>> iTerm2 + zsh
+echo "Installing zsh..."
+if [ -L "$HOME/.config/zsh" ]; then
+    rm "$HOME/.config/zsh"
+fi
+ln -sf "$DOTFILES_DIR/zsh" "$HOME/.config/zsh"
+source "$DOTFILES_DIR/zsh/install.sh"
+
+#-->>>>>> SketchyBar
 echo "Installing SketchyBar..."
+if [ -L "$HOME/.config/sketchybar" ]; then
+    rm "$HOME/.config/sketchybar"
+fi
 ln -sf "$DOTFILES_DIR/sketchybar" "$HOME/.config/sketchybar"
 source "$DOTFILES_DIR/sketchybar/install.sh"
 
+#-->>>>>> tmux
 echo "Installing tmux..."
+if [ -L "$HOME/.config/tmux" ]; then
+    rm "$HOME/.config/tmux"
+fi
 ln -sf "$DOTFILES_DIR/tmux" "$HOME/.config/tmux"
 source "$DOTFILES_DIR/tmux/install.sh"
+
---
+++ zsh/install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Install dependencies
+
+# Install iTerm2
+if ! command_exists iterm2; then
+    echo "Installing iTerm2..."
+    brew install --cask iterm2
+else
+    echo "iTerm2 is already installed."
+fi
+
+# Check if Oh My Zsh is installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is not installed. Installing..."
+
+    # Install Oh My Zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "Oh My Zsh is already installed."
+fi
+
+# Install PowerLevel10K Theme
+POWERLEVEL10K_DIR="${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k"
+if [ ! -d "$POWERLEVEL10K_DIR" ]; then
+    echo "Installing PowerLevel10K theme..."
+    git clone https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_DIR"
+else
+    echo "PowerLevel10K is already installed."
+fi
+# Install Meslo Nerd Font
+if ! fc-list | grep -i "Meslo" > /dev/null; then
+    echo "Meslo Nerd Font is not installed. Installing..."
+    brew install --cask font-meslo-nerd-font
+else
+    echo "Meslo Nerd Font is already installed."
+fi
+
+# Install Zsh Plugins (zsh-autosuggestions, zsh-syntax-highlighting)
+ZSH_PLUGINS_DIR="${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins"
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions plugin..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_PLUGINS_DIR/zsh-autosuggestions"
+else
+    echo "zsh-autosuggestions plugin is already installed."
+fi
+
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting plugin..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting"
+else
+    echo "zsh-syntax-highlighting plugin is already installed."
+fi
